b89955b061341edc95aa947bb7f6c763c9162b63
mauroduranpelio <mauduran@gmail.com>
2019-06-19 23:26:44 -0400

    Third commit

--- digit_verif.R
+++ digit_verif.R
@@ -5,16 +5,16 @@
 
 digit_verif <- function(RUT){
   
-  # validar que input RUT sea numerico y con formato: 1000000<=RUT<=99999999
+  # valido que input RUT sea numerico y con formato: 1000000<=RUT<=99999999
   if( (!is.numeric(RUT)) | (RUT < 1000000) | (RUT > 99999999)) {
     stop("Rut ingresado debe ser un número entre 1.000.000 y 99.999.999")}
   
   # agregar comentario explicativo:
   largo_rut <- nchar(RUT)
-  new_rut <- strsplit(as.character(RUT),"")[[1]]
-  new_rut <- as.numeric(new_rut[largo_rut:1])
+  new_rut <- strsplit(as.character(RUT),"")[[1]] # separo digitos del input RUT en un char_vector
+  new_rut <- as.numeric(new_rut[largo_rut:1]) # revierto orden de los digitos y convierto en num_vector
   
-  suma_prod <- suppressWarnings(sum(new_rut*c(2:7))) # ver cómo evitar mensaje warning
+  suma_prod <- suppressWarnings(sum(new_rut*c(2:7))) # realizo suma_producto según algoritmo y suprimo mensaje warning
   digit <- 11 - suma_prod%%11
   
   if(digit == 11) {return(0)}
